e9477759d789cc21a63394841fb17286bc03872d
Brenda Jiménez <brenjitec1@live.com>
2020-12-18 14:38:54 -0600

    docs agregado

---
+++ docs/precache-manifest.b784fccbfc5e9372efd5d8126b710abc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b58af11063d08e5639f5e2c63c4bc6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e876ca8b5f10f718b686",
+    "url": "/static/css/main.0424cbb2.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e876ca8b5f10f718b686",
+    "url": "/static/js/main.dd15fbd9.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
